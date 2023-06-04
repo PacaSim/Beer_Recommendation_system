4955412a8eb93ca9db2f03060ed08348dbf7775a
PacaSim <dmg919@naver.com>
2023-06-04 23:42:36 +0900

    로그인 회원가입 post 확인

--- Web/server.py
+++ Web/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import numpy as np
+import pandas as pd
 import joblib
 
 app = Flask(__name__)
@@ -8,12 +9,18 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-   return render_template('register.html')
+   beer_list = pd.read_csv('beer_list.csv', encoding='utf-8', index_col=0)
+   beer_list = beer_list['맥주']
+   data = request.form
+   print(data)
+   return render_template('register.html',beer_list=beer_list)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+   data1 = request.form
+   print(data1)
    return render_template('login.html')
 
 if __name__ == '__main__':
